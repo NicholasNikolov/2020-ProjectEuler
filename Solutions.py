ea792f80804f5734964e55a7a9f25ac99cc2e617
Nicklovn <nikolovnickv@gmail.com>
2019-12-21 17:00:25 -0600

    First 5 problems solved
    There are fun!

--- Solutions.py
+++ Solutions.py
@@ -15,6 +15,7 @@ print("Problem 1: " , x)
 
 # Find sum of even-valued terms in Fibonacci sequence
 # whose values do not exceed four million.
+
 first = 1
 second = 2
 fib = [1,2]
@@ -33,3 +34,58 @@ for i in fib:
 
 print("Problem 2: " , SumOfEvens)
         
+
+# Find the prime factorization of 600851475143
+
+composite = 600851475143
+p = 2
+factorization = []
+
+while composite >= p:
+    if composite%p == 0:
+        factorization.append(p)
+        composite /= p
+    else:
+        p += 1
+        
+print(factorization)
+
+# Find the largest palindromic number made from the
+# product of two 3-digit numbers
+
+
+largestPally = 9009
+for i in range(999,100,-1):
+    for j in range(999,100,-1):
+        pally = i*j
+        if str(pally) == str(pally)[::-1]:
+            if pally > largestPally:
+                largestPally = pally
+
+print("Largest Pally: ",largestPally)
+
+# Smallest positive number that is evenly divisible by all
+# number from 1 to 20
+# Note. This is the first problem to take a few seconds to
+# solve. Therefore there may be grounds for efficiency improvements
+
+condition = False
+smallestNumb = 20
+while condition == False:
+    tester = 0
+    for i in range(1,21):
+        if smallestNumb %i == 0:
+            tester += 1
+    if tester == 20:
+        condition = True
+        break
+    smallestNumb += 20
+    
+print("Problem 5: ",smallestNumb)
+    
+    
+    
+    
+    
+
+
